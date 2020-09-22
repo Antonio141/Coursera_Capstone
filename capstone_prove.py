17d3924cdf0a0feaeff58da44bc0b916d0aa0959
Antonio <imperio.antonio@gmail.com>
2020-09-22 10:48:16 +0200

    commit changes

---
+++ capstone_prove.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('D:/personale/MACHINELEARNING/IBM/capstone/Data-Collisions.csv')
+
+print(df['JUNCTIONTYPE'].unique())
