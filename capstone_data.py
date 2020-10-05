d0728a00ad64212c2c56fb516d610ed58e1d1c89
Antonio <imperio.antonio@gmail.com>
2020-10-06 01:47:34 +0200

    final

--- capstone_data.py
+++ capstone_data.py
@@ -15,7 +15,7 @@ import model
 # Call the dataframe
 # df.to_csv('D:/personale/MACHINELEARNING/IBM/capstone/df_district_2.csv')
 
-df = pd.read_csv('D:/personale/MACHINELEARNING/IBM/capstone/df_district_4.csv')
+df = pd.read_csv('D:/personale/MACHINELEARNING/IBM/capstone/file_for_graphs.csv')
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
@@ -28,32 +28,43 @@ pd.set_option('display.max_columns', None)
 # clean_data.transform(df,pd,np)
 # clean_data.dummies(df,pd)
 
-# col = 'DISTRICT'
-# clean_data.fill_na_by_percentage(df,np, col)
 
 # df.drop(['JUNCTIONTYPE', 'SDOT_COLDESC', 'UNDERINFL', 'ST_COLDESC','HITPARKEDCAR', 'geometry','Unnamed: 0','Unnamed: 0.1'], axis = 1, inplace = True)
-df.drop('Unnamed: 0', axis = 1, inplace = True)
+# df.drop('Unnamed: 0', axis = 1, inplace = True)
+# df['PERSONCOUNT'][df['PERSONCOUNT']>= 6] = 'Six or more'
 
+# df['PERSONCOUNT'].replace( \
+# to_replace=(0,1,2,3,4,5), \
+# value=('Zero','One','Two','Three','Four','Five'), \
+# inplace=True)
 
-# df.to_csv('D:/personale/MACHINELEARNING/IBM/capstone/df_district_4.csv')
+temp= df.groupby(['ROADCOND'])['SEVERITYCODE'].value_counts(normalize=True).mul(100).rename('percentage').reset_index().sort_index()
 
-# print(df.info())
-model.predict(df,np)
+plt.figure(figsize=(7,7))
+chart = sns.barplot(data = temp, x = 'percentage', y= 'ROADCOND', hue= 'SEVERITYCODE', linewidth=2,
+palette='PuOr')
+# , order=('Zero','One','Two','Three','Four','Five','Six or more')
+chart.set_title('SEVERITY BY ROAD CONDITIONS', weight='bold')
+chart.set_xticklabels(chart.get_xticklabels(), rotation=45, horizontalalignment='right')
+chart.set_yticklabels(chart.get_yticklabels(), rotation=0, horizontalalignment='right', style='italic')
+chart.set_xlabel('Percentage')
+chart.set_ylabel('Road Conditions')
+chart.set_xbound(upper=115)
+# chart.set_ybound(lower=-1,upper=10)
 
-# df['SEVERITYCODE'].replace(to_replace=(1,2), value=(0,1))
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
-# y = df['SEVERITYCODE']
+total=100
+new_value=0.5
+for p in chart.patches:
+        percent = '{:.1f}%'.format(100 * p.get_width()/total)
+        x =  p.get_width() + .5
+        y = p.get_y() + p.get_height() - .15 
+        chart.annotate(percent, (x, y), fontsize=10, color='k', weight='bold', style='italic')
 
-# df.drop('SEVERITYCODE', axis= 1, inplace= True)
+# df['PERSONCOUNT'].replace( \
+# value=(0,1,2,3,4,5,'6 or more'), \
+# to_replace=('Zero','One','Two','Three','Four','Five','Six or more'), \
+# inplace=True)
 
-# X_train, X_test, y_train, y_test = train_test_split( df, y, test_size=0.2, random_state=4)
-
-
-
-# # C = 1/lambda (Regularization Parameter) solver: Method of resolution of the algorithm
-# LR = LogisticRegression(C=0.01, solver='liblinear').fit(X_train,y_train)
-# yhat = LR.predict(X_test)
-# yhat_prob = LR.predict_proba(X_test)
-
-# lr_a = metrics.accuracy_score(y_test, yhat)
-# print('Logistic Regression accuracy: ', lr_a)
+plt.show()
